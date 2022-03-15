1de230d74fa26f5e4faf1e4650d5b0f73de3cf55
Luigi <luigi.drnasin12@gmail.com>
2022-03-15 13:48:50 +0100

    Sketck pad

--- javascript.js
+++ javascript.js
@@ -1,35 +1,48 @@
 let size = 16;
 let board = document.querySelector(".board");
 
-board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
-board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
-
-for (let i = 0; i < 256; i++){
-    let square = document.createElement("div");
-    square.style.backgroundColor = "blue";
-    board.insertAdjacentElement("beforeend", square);
-}
-
-
-
 board.addEventListener("mouseover", function(event){
-    event.target.style.backgroundColor = "orange";
+    event.target.style.backgroundColor = "blue";
 })
 
+function makeGrid(){
+    board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+    for (let i = 0; i < 256; i++){
+        let square = document.createElement("div");
+        square.style.backgroundColor = "white";
+        board.insertAdjacentElement("beforeend", square);
+    }
+}
+
 function reset(){
-    let board = document.querySelector(".board");
+    var newSize = window.prompt("Enter the squares per side: ");
+    while (newSize <= 10 || newSize >= 100){
+        alert("Invalid size")
+        var newSize = window.prompt("Enter the squares per side: ");
+    }
     let squares = board.querySelectorAll("div");
-    squares.forEach((div) => (div.style.backgroundColor = "blue"));
+    squares.forEach((div) => (div.style.backgroundColor = "white"));
+    board.style.gridTemplateColumns = `repeat(${newSize} , 1fr)`;
+    board.style.gridTemplateRows = `repeat(${newSize} , 1fr)`;
+
+    for (let i = 0; i < newSize * newSize; i++){
+        let square = document.createElement("div");
+        square.style.backgroundColor = "white";
+        board.insertAdjacentElement("beforeend", square);
+    }
 }
 
-function red(){
-    let board = document.querySelector(".board");
-    let squares = board.querySelectorAll("div");
-    squares.forEach((div) => (div.style.backgroundColor = "red"));
+function color(){
+    var colors = ["red", "orange", "yellow", "green"];
+    var randColor = colors[Math.floor(Math.random() * colors.length)];
+    board.addEventListener("mouseover", function(event){
+    event.target.style.backgroundColor = randColor;
+    })
 }
 
-function black(){
-    let board = document.querySelector(".board");
-    let squares = board.querySelectorAll("div");
-    squares.forEach((div) => (div.style.backgroundColor = "black"));
+function eraser(){
+    board.addEventListener("mouseover", function(event){
+    event.target.style.backgroundColor = "white";
+    })
 }
